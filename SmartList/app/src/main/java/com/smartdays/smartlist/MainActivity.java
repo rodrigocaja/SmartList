78f442d816b25de8073e0232c7518059f8bc9223
Rodrigo Caja <rodrigoccaja@gmail.com>
2017-01-10 23:47:28 -0200

    That's all for today folks

--- SmartList/app/src/main/java/com/smartdays/smartlist/MainActivity.java
+++ SmartList/app/src/main/java/com/smartdays/smartlist/MainActivity.java
@@ -1,23 +1,16 @@
 package com.smartdays.smartlist;
 
-import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -57,6 +50,7 @@ public class MainActivity extends AppCompatActivity
 
         DbHandle dbHandle = new DbHandle();
         dbHandle.criaDB(db);
+        db.close();
     }
 
     @Override
@@ -69,6 +63,7 @@ public class MainActivity extends AppCompatActivity
             Log.v("DEBUG", String.valueOf(count));
             if (count == 0) {
                 super.onBackPressed();
+
             } else {
                 getFragmentManager().popBackStack();
             }
